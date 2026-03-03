810b0b6c3fc0c5abd1e8dba17e20f3be4b0fc6f3
Kafi <it'skafi009@gmail.com>
2026-03-03 22:20:17 +0530

    Force update AgentProps

--- components/FormField.tsx
+++ components/FormField.tsx
@@ -1,37 +1,22 @@
-import React from 'react'
-import {FormControl, FormDescription, FormItem, FormLabel, FormMessage} from "@/components/ui/form";
-import {Input} from "@/components/ui/input";
-import {Controller, FieldValues, Path, Control} from "react-hook-form";
+import React from "react";
+import { Controller, FieldValues, Path, Control, ControllerRenderProps, ControllerFieldState, UseFormStateReturn } from "react-hook-form";
 
 interface FormFieldProps<T extends FieldValues> {
     control: Control<T>;
     name: Path<T>;
-    label: string;
-    placeholder?: string;
-    type?: 'text' | 'email' | 'password' | 'file';
+    render: (props: {
+        field: ControllerRenderProps<T, Path<T>>;
+        fieldState: ControllerFieldState;
+        formState: UseFormStateReturn<T>;
+    }) => React.ReactElement;
 }
 
-// ✅ Generic Function Component
 const FormField = <T extends FieldValues>({
                                               control,
                                               name,
-                                              label,
-                                              placeholder,
-                                              type = "text"
-                                          }: FormFieldProps<T>) => (
-    <Controller
-        name={name}
-        control={control}
-        render={({ field }) => (
-            <FormItem>
-                <FormLabel className="label">{label}</FormLabel>
-                <FormControl>
-                    <Input className="input" placeholder={placeholder} {...field} type={type} />
-                </FormControl>
-                <FormMessage />
-            </FormItem>
-        )}
-    />
-);
+                                              render,
+                                          }: FormFieldProps<T>) => {
+    return <Controller name={name} control={control} render={render} />;
+};
 
 export default FormField;
