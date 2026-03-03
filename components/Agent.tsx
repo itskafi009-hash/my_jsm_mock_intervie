3ec153e366920309eec843f77f07614437b8a7ca
Kafi <it'skafi009@gmail.com>
2026-03-03 20:48:07 +0530

    Fix AgentProps type and add userId/type

--- components/Agent.tsx
+++ components/Agent.tsx
@@ -16,7 +16,7 @@ interface AgentProps{
     type: string;
 }
 
-const Agent = ({ userName, userId, type }: any) => {
+const Agent = ({ userName, userId, type }: AgentProps) => {
 
     const [callStatus, setCallStatus] = useState<CallStatus>(CallStatus.INACTIVE);
     const isSpeaking = true;
