60a19788933350f4821801d00007e343ebf626a5
Kafi <it'skafi009@gmail.com>
2026-03-05 14:47:52 +0530

    fix firebase admin structure

--- firebase/admin.ts
+++ firebase/admin.ts
@@ -6,11 +6,17 @@ import { getFirestore } from "firebase-admin/firestore";
 
 const initFirebaseAdmin = () => {
     if (!getApps().length) {
+        const privateKey = process.env.FIREBASE_PRIVATE_KEY;
+
+        if (!privateKey) {
+            throw new Error("FIREBASE_PRIVATE_KEY is missing");
+        }
         initializeApp({
             credential: cert({
                 projectId: process.env.FIREBASE_PROJECT_ID!,
                 clientEmail: process.env.FIREBASE_CLIENT_EMAIL!,
-                privateKey: process.env.FIREBASE_PRIVATE_KEY!.replace(/\\n/g, "\n"),
+               // privateKey: process.env.FIREBASE_PRIVATE_KEY!.replace(/\\n/g, "\n"),
+                privateKey: privateKey.replace(/\\n/g, "\n"),
             }),
         });
     }
