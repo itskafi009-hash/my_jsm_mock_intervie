fb414c7651ec61a836e476976331bea27ed66ce3
Kafi <it'skafi009@gmail.com>
2026-03-12 11:09:09 +0530

    show generated interviews on the home page

--- components/DisplayTechIcons.tsx
+++ components/DisplayTechIcons.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+/*import React from 'react'
 import {cn, getTechLogos} from "@/lib/utils";
 import Image from "next/image";
 
@@ -17,5 +17,36 @@ const DisplayTechIcons = async ({techStack }: TechIconProps) => {
         ))}</div>
     )
 }
-export default DisplayTechIcons//
+export default DisplayTechIcons//*/
+
+import Image from "next/image";
+import { cn,getTechLogos } from "@/lib/utils";
+
+const DisplayTechIcons = ({ techIcons }: any) => {
+    return (
+        <div className="flex flex-row">
+            {techIcons?.slice(0, 3).map(({ tech, url }: any, index: number) => (
+                <div
+                    key={tech}
+                    className={cn(
+                        "relative group bg-dark-300 rounded-full p-2 flex-center",
+                        index >= 1 && "-ml-3"
+                    )}
+                >
+                    <span className="tech-tooltip">{tech}</span>
+
+                    <Image
+                        src={url}
+                        alt={tech}
+                        width={20}
+                        height={20}
+                        className="size-5"
+                    />
+                </div>
+            ))}
+        </div>
+    );
+};
+
+export default DisplayTechIcons;
 
